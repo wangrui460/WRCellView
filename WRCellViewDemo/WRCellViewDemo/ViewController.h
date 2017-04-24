6c5dc0429a5a89ec61f321648d1ca23310d33503
wangrui460 <wangruidev@gmail.com>
2017-04-24 15:01:05 +0800

    init WRCellViewDemo

---
+++ WRCellViewDemo/WRCellViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WRCellViewDemo
+//
+//  Created by wangrui on 2017/4/24.
+//  Copyright © 2017年 wangrui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
