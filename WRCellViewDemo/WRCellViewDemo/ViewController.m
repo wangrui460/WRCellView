e079e5b24d9af51f1462f3aa99eedcd4e32ccba4
wangrui460 <wangruidev@gmail.com>
2017-04-24 18:47:57 +0800

    自定义View：WRCellView基本完成

--- WRCellViewDemo/WRCellViewDemo/ViewController.m
+++ WRCellViewDemo/WRCellViewDemo/ViewController.m
@@ -7,23 +7,47 @@
 //
 
 #import "ViewController.h"
+#import "WRCellView.h"
+
+#define kScreenWidth      [UIScreen mainScreen].bounds.size.width
 
 @interface ViewController ()
-
+@property (nonatomic, strong) WRCellView* shakeView;
+@property (nonatomic, strong) WRCellView* inviteFriendView;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self.view addSubview:self.shakeView];
+    [self.view addSubview:self.inviteFriendView];
+    
+    self.shakeView.frame = CGRectMake(0, 64, kScreenWidth, 50);
+    self.inviteFriendView.frame = CGRectMake(0, _shakeView.frame.origin.y+_shakeView.bounds.size.height, kScreenWidth, 50);
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (WRCellView *)shakeView {
+    if (_shakeView == nil) {
+        _shakeView = [[WRCellView alloc] initWithLineStyle:WRCellStyleIconLabel_LabelIndicator];
+        _shakeView.leftIcon.image = [UIImage imageNamed:@"shakeBonus"];
+        _shakeView.leftLabel.text = @"摇红包";
+        _shakeView.rightLabel.text = @"偷点地主家的余粮";
+    }
+    return _shakeView;
 }
 
+- (WRCellView *)inviteFriendView {
+    if (_inviteFriendView == nil) {
+        _inviteFriendView = [[WRCellView alloc] initWithLineStyle:WRCellStyleIconLabel_LabelIndicator];
+        _inviteFriendView.leftIcon.image = [UIImage imageNamed:@"myFriendIcon"];
+        _inviteFriendView.leftLabel.text = @"邀请好友";
+        _inviteFriendView.rightLabel.text = @"立得10元50%红包";
+        _inviteFriendView.rightLabel.textColor = [UIColor redColor];
+    }
+    return _inviteFriendView;
+}
 
 @end
---
+++ WRCellViewDemo/WRCellViewDemo/WRCellView/WRCellView.m
@@ -0,0 +1,302 @@
+//
+//  WRCellView.m
+//  WRCellViewDemo
+//
+//  Created by wangrui on 16/4/26.
+//  Copyright © 2016年 wangrui. All rights reserved.
+//
+
+#import "WRCellView.h"
+#import <objc/runtime.h>
+
+#define kScreenWidth      [UIScreen mainScreen].bounds.size.width
+#define kSelfHeight       self.bounds.size.height
+#define kBottomLineHeight 0.5
+#define kMargin           16
+#define kPadding          6
+#define kLeftLabelFont    16
+#define kRightLabelFont   14
+
+static char kActionHandlerTapBlockKey;
+static char kActionHandlerTapGestureKey;
+
+UIColor *CellSelectedColor  = nil;
+UIColor *CellNormalColor    = nil;
+UIColor *CellSegmentColor   = nil;
+UIColor *CellLeftTextColor  = nil;
+UIColor *CellRightTextColor = nil;
+
+@interface WRCellView ()
+@property (nonatomic, assign) WRCellStyle style;
+@property (nonatomic, strong) UIView *lastView;    // 类似中间件，中转作用
+@property (nonatomic, strong) UIView *line;
+@property (nonatomic, strong, readwrite) UIImageView *leftIcon;
+@property (nonatomic, strong, readwrite) UILabel *leftLabel;
+@property (nonatomic, strong, readwrite) UIImageView *rightIcon;
+@property (nonatomic, strong, readwrite) UILabel *rightLabel;
+@property (nonatomic, strong, readwrite) UIImageView *rightIndicator;
+@property (nonatomic, assign, readwrite) BOOL isLast;
+@end
+
+@implementation WRCellView
+
++ (void)load
+{
+    CellSelectedColor  = [UIColor colorWithRed:230/255.0 green:230/255.0 blue:230/255.0 alpha:1.0];
+    CellNormalColor    = [UIColor whiteColor];
+    CellSegmentColor   = [UIColor colorWithRed:218/255.0 green:218/255.0 blue:218/255.0 alpha:1.0];
+    CellLeftTextColor  = [UIColor colorWithRed:51/255.0 green:51/255.0 blue:51/255.0 alpha:1.0];
+    CellRightTextColor = [UIColor colorWithRed:155/255.0 green:155/255.0 blue:155/255.0 alpha:1.0];
+}
+
+- (instancetype)initWithFrame:(CGRect)frame lineStyle:(WRCellStyle)style
+{
+    if (self = [super initWithFrame:frame])
+    {
+        _style = style;
+        [self setupView];
+        __weak typeof(self) weakSelf = self;
+        [self setTapActionWithBlock:^
+        {
+            __strong typeof(self) pThis = weakSelf;
+            pThis.backgroundColor = CellSelectedColor;
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^
+            {
+                if (pThis.tapBlock)
+                {
+                    pThis.tapBlock();
+                    [pThis touchesCancelled:[NSSet new] withEvent:nil];
+                }
+            });
+        }];
+    }
+    return self;
+}
+
+- (instancetype)initWithLineStyle:(WRCellStyle)style
+{
+    if (self = [super init])
+    {
+        _style = style;
+        [self setupView];
+        __weak typeof(self) weakSelf = self;
+        [self setTapActionWithBlock:^
+        {
+            __strong typeof(self) self = weakSelf;
+            self.backgroundColor = CellSelectedColor;
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^
+            {
+                if (self.tapBlock)
+                {
+                    self.tapBlock();
+                    [self touchesCancelled:[NSSet new] withEvent:nil];
+                }
+            });
+        }];
+    }
+    return self;
+}
+
+- (void)setLineStyleWithLeftZero
+{
+    self.line.frame = CGRectMake(0, kSelfHeight - 0.5, kScreenWidth, kBottomLineHeight);
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.backgroundColor = CellSelectedColor;
+}
+
+// 当取消选中后
+-(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.backgroundColor = CellNormalColor;
+}
+
+// 当从其他界面回到这个界面后，这个view处于未选中状态
+-(void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.backgroundColor = CellNormalColor;
+}
+
+- (void)setupView
+{
+    self.backgroundColor = CellNormalColor;
+    //----------------------- 左侧
+    if (self.style & 0x10000) {
+        [self addSubview:self.leftIcon];
+    }
+    if (self.style & 0x1000) {
+        [self addSubview:self.leftLabel];
+    }
+    
+    //----------------------- 右侧
+    if (self.style & 0x1) {
+        [self addSubview:self.rightIndicator];
+        _lastView = self.rightIndicator;
+    }
+    if (self.style & 0x10) {
+        [self addSubview:self.rightIcon];
+        _lastView = self.rightIcon;
+    }
+    if (self.style & 0x100) {
+        [self addSubview:self.rightLabel];
+    }
+
+    self.line = [[UIView alloc] init];
+    self.line.backgroundColor = CellSegmentColor;
+    [self addSubview:self.line];
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    //----------------------- 左侧
+    if (self.style & 0x10000)
+    {
+        CGFloat leftIconWidth  = self.leftIcon.image.size.width;
+        CGFloat leftIconHeight = self.leftIcon.image.size.height;
+        CGFloat leftIconY = (kSelfHeight - leftIconHeight - kBottomLineHeight) / 2.0;
+        self.leftIcon.frame = CGRectMake(kMargin, leftIconY, leftIconWidth, leftIconHeight);
+    }
+    
+    if (self.style & 0x1000)
+    {
+        CGSize leftLabelSize = [self.leftLabel.text sizeWithAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:kLeftLabelFont]}];
+        CGFloat leftLabelWidth  = leftLabelSize.width;
+        CGFloat leftLabelHeight = leftLabelSize.height;
+        if (self.leftIcon)
+        {
+            CGFloat leftLabelX = self.leftIcon.frame.origin.x + self.leftIcon.bounds.size.width + kPadding;
+            CGFloat leftLabelY = (kSelfHeight - leftLabelHeight - kBottomLineHeight) / 2.0;
+            self.leftLabel.frame = CGRectMake(leftLabelX, leftLabelY, leftLabelWidth, leftLabelHeight);
+        }
+        else
+        {
+            CGFloat leftLabelY = (kSelfHeight - leftLabelHeight - kBottomLineHeight) / 2.0;
+            self.leftLabel.frame = CGRectMake(kMargin, leftLabelY, leftLabelWidth, leftLabelHeight);
+        }
+    }
+    
+    
+    //----------------------- 右侧
+    if (self.style & 0x1)
+    {
+        CGFloat rightIndicatorWidth  = self.rightIndicator.image.size.width;
+        CGFloat rightIndicatorHeight = self.rightIndicator.image.size.height;
+        CGFloat rightIndicatorX = kScreenWidth - kMargin - rightIndicatorWidth;
+        CGFloat rightIndicatorY = (kSelfHeight - rightIndicatorHeight - kBottomLineHeight) / 2.0;
+        self.rightIndicator.frame = CGRectMake(rightIndicatorX, rightIndicatorY, rightIndicatorWidth, rightIndicatorHeight);
+    }
+    
+    if (self.style & 0x10)
+    {
+        CGFloat rightIconWidth  = self.rightIcon.image.size.width;
+        CGFloat rightIconHeight = self.rightIcon.image.size.height;
+        if (self.lastView)
+        {
+            CGFloat rightIconX = self.lastView.frame.origin.x - kPadding - rightIconWidth;
+            CGFloat rightIconY = (kSelfHeight - rightIconHeight - kBottomLineHeight) / 2.0;
+            self.rightIcon.frame = CGRectMake(rightIconX, rightIconY, rightIconWidth, rightIconHeight);
+        }
+        else
+        {
+            CGFloat rightIconX = kScreenWidth - kMargin - rightIconWidth;
+            CGFloat rightIconY = (kSelfHeight - rightIconHeight - kBottomLineHeight) / 2.0;
+            self.rightIcon.frame = CGRectMake(rightIconX, rightIconY, rightIconWidth, rightIconHeight);
+        }
+    }
+    
+    if (self.style & 0x100)
+    {
+        CGSize rightLabelSize = [self.rightLabel.text sizeWithAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:kRightLabelFont]}];
+        CGFloat rightLabelWidth  = rightLabelSize.width;
+        CGFloat rightLabelHeight = rightLabelSize.height;
+        if (self.lastView)
+        {
+            CGFloat rightLabelX = self.lastView.frame.origin.x - kPadding - rightLabelWidth;
+            CGFloat rightLabelY = (kSelfHeight - rightLabelHeight - kBottomLineHeight) / 2.0;
+            self.rightLabel.frame = CGRectMake(rightLabelX, rightLabelY, rightLabelWidth, rightLabelHeight);
+        }
+        else
+        {
+            CGFloat rightLabelX = kScreenWidth - kMargin - rightLabelWidth;
+            CGFloat rightLabelY = (kSelfHeight - rightLabelHeight - kBottomLineHeight) / 2.0;
+            self.rightLabel.frame = CGRectMake(rightLabelX, rightLabelY, rightLabelWidth, rightLabelHeight);
+        }
+    }
+    
+    if (self.isLast) {
+        self.line.frame = CGRectMake(0, kSelfHeight - 0.5, kScreenWidth, kBottomLineHeight);
+    }
+    else {
+        self.line.frame = CGRectMake(kMargin, kSelfHeight - 0.5, kScreenWidth, kBottomLineHeight);
+    }
+}
+
+#pragma mark -  设置简单的轻点 block事件
+- (void)setTapActionWithBlock:(void (^)(void))block
+{
+    UITapGestureRecognizer *gesture = objc_getAssociatedObject(self, &kActionHandlerTapGestureKey);
+    if (!gesture)
+    {
+        gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleActionForTapGesture:)];
+        [self addGestureRecognizer:gesture];
+        objc_setAssociatedObject(self, &kActionHandlerTapGestureKey, gesture, OBJC_ASSOCIATION_RETAIN);
+    }
+    objc_setAssociatedObject(self, &kActionHandlerTapBlockKey, block, OBJC_ASSOCIATION_COPY);
+}
+
+- (void)handleActionForTapGesture:(UITapGestureRecognizer *)gesture
+{
+    if (gesture.state == UIGestureRecognizerStateRecognized)
+    {
+        void(^action)(void) = objc_getAssociatedObject(self, &kActionHandlerTapBlockKey);
+        if (action) {
+            action();
+        }
+    }
+}
+
+#pragma mark - getter / setter
+- (void)setHideBottomLine:(BOOL)hideBottomLine {
+    _line.hidden = hideBottomLine;
+}
+
+- (UIImageView *)leftIcon {
+    if (_leftIcon == nil) {
+        _leftIcon = [UIImageView new];
+    }
+    return _leftIcon;
+}
+
+- (UILabel *)leftLabel {
+    if (_leftLabel == nil) {
+        _leftLabel = [UILabel new];
+        _leftLabel.font = [UIFont systemFontOfSize:kLeftLabelFont];
+        _leftLabel.textColor = CellLeftTextColor;
+    }
+    return _leftLabel;
+}
+
+- (UIImageView *)rightIndicator {
+    if (_rightIndicator == nil) {
+        _rightIndicator = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"check_btn"]];
+    }
+    return _rightIndicator;
+}
+
+- (UIImageView *)rightIcon {
+    if (_rightIcon == nil) {
+        _rightIcon = [UIImageView new];
+    }
+    return _rightIcon;
+}
+
+- (UILabel *)rightLabel {
+    if (_rightLabel == nil) {
+        _rightLabel = [UILabel new];
+        _rightLabel.font = [UIFont systemFontOfSize:kRightLabelFont];
+        _rightLabel.textColor = CellRightTextColor;
+    }
+    return _rightLabel;
+}
+
+@end
